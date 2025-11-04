b273262ecc8b5e3569d8f6415b5d7ac93a043935
Aiveriwhair <aiveriwhair@gmail.com>
2025-11-04 23:31:19 +0100

    init

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-config-prettier';
+
+export default [
+    {
+        ignores: ['dist/**', 'node_modules/**', 'eslint.config.js'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                project: './tsconfig.json',
+                sourceType: 'module',
+                ecmaVersion: 'latest',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            import: importPlugin,
+        },
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'import/order': ['warn', { 'newlines-between': 'always' }],
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
+    },
+    prettierPlugin,
+];

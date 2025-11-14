b0213489db89d9547343c538e2c4e1295e5d83a6
Aiveriwhair <aiveriwhair@gmail.com>
2025-11-14 20:06:14 +0100

    impl: improve routing

--- src/modules/maintenance/index.ts
+++ src/modules/maintenance/index.ts
@@ -2,11 +2,14 @@ import fp from 'fastify-plugin';
 import maintenanceRoutes from './maintenance.route';
 import maintenancePlugin from './maintenance.plugin';
 
+export const DEFAULT_MODULE_PREFIX = '/api/maintenance';
+
 export interface MaintenancePluginOptions {
     ignoreRoutes?: string[];
+    prefix?: string;
 }
 
 export const maintenanceModule = fp(async (fastify, opts: MaintenancePluginOptions) => {
     await fastify.register(maintenancePlugin, opts);
-    await fastify.register(maintenanceRoutes, { prefix: '/maintenance' });
+    await fastify.register(maintenanceRoutes, { prefix: opts.prefix ?? DEFAULT_MODULE_PREFIX });
 });
--- src/modules/monitoring/index.ts
+++ src/modules/monitoring/index.ts
@@ -3,7 +3,11 @@ import fp from 'fastify-plugin';
 import monitoringRoutes from './monitoring.route';
 import monitoringPlugin from './monitoring.plugin';
 
-export const monitoringModule = fp(async (fastify) => {
+export interface MonitoringPluginOptions {
+    prefix?: string;
+}
+
+export const monitoringModule = fp(async (fastify, opts: MonitoringPluginOptions) => {
     await fastify.register(monitoringPlugin);
-    await fastify.register(monitoringRoutes, { prefix: '/monitoring' });
+    await fastify.register(monitoringRoutes, { prefix: opts.prefix ?? '/monitoring' });
 });
